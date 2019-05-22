82f1a579d491f2d97f726a39ad555dee45ffc3f1
dyb881 <15118995517@163.com>
2019-05-22 17:12:49 +0800

    使用自定义脚手架

---
+++ README.md
@@ -0,0 +1,215 @@
+# fetch-request
+
+配置型请求封装，所有参数最终均合成配置对象，并使用 fetch 发出请求
+
+## DEMO
+
+```
+import FetchReques, { log } from '@dyb881/fetch-request';
+
+/**
+ * 成功状态码
+ */
+const successCode = [0, 200, '0000', 1000, 1001, 1002, 1003, 1004, 1005];
+
+/**
+ * 请求模块初始化并输出请求方法以及参数
+ */
+export const { baseURL, get, post, put, patch, del, upload } = new FetchReques({
+  host: 'http://192.168.0.74:8002', // 服务器地址
+  apiPath: '/api', // API地址基础目录
+  interceptorsRequest: config => {
+    log.request(config);
+    return config;
+  },
+  interceptorsResponse: (res, config) => {
+    if (!res.errorText && successCode.indexOf(res.code) === -1) {
+      res.error = res.code;
+      res.errorText = res.msg || '请求异常';
+    }
+    res.ok = !res.errorText; // 请求结果状态 成功/失败
+    log.response(res, config, res.ok);
+    return res;
+  },
+});
+
+```
+
+## 使用方法
+
+例：
+
+```
+/**
+ * new FetchRequest(config?: IFetchRequestConfig) 用于生成请求方法，余下为默认配置下生成导出的请求方法
+ * 注！ 除了 del 对应 DELETE，其他请求方法均对应大写的 method
+ */
+import FetchRequest, { get, post, put, patch, del, upload } from '@dyb881/fetch-request';
+
+/**
+ * 请求方法(url: 请求地址, data?: 请求数据, config?: 请求配置(IConfig) | 请求标签(string)) => Promise<any>
+ */
+// 直接使用地址请求
+get('http://localhost/api/test?id=1000');
+
+// 请求数据 data 会自动叠加到请求地址 url，
+get('http://localhost/api/test',{ id: 1000 });
+
+// 请求标签会自动转化至请求配置
+get('http://localhost/api/test',{ id: 1000 }, '测试请求');
+
+// 由于所有参数均会合并到配置对象，请求配置甚至可以覆盖 请求地址 url 和 请求数据 data
+get('http://localhost/api/test',{ id: 1000 }, {
+  label: '测试请求',
+  timeout: 5000,
+  url: 'http://localhost/api/test/new',
+  data: {
+    id: 2000
+  }
+});
+
+// 追加覆盖配置
+get('http://localhost/api/test',{ id: 1000 }, '测试请求', {
+  label: '新测试请求',
+});
+
+// 除了 get 和 upload 以外的请求方法中的 请求数据 data 会根据 headers['Content-type'] 的类型处理提交数据
+// 如 application/json 会自动 JSON.stringify 处理请求数据
+post('http://localhost/api/test',{ id: 1000 });
+
+// upload 会把请求数据写入 FormData 对象，放入 body，并清空 headers，用于上传文件
+// 后端可直接使用 file 字段获取文件
+upload('http://localhost/api/upload', { file: File }, '上传文件');
+
+/**
+ * 生成请求方法
+ * 基础路径 baseURL = host + apiPath
+ * 配置覆盖等级 默认配置 < 初始化请求配置 < 请求方法配置
+ */
+const { baseURL, get, post, put, patch, del, upload } = new FetchRequest({
+  host: 'http://localhost',
+  apiPath: '/api',
+  // 请求配置
+  defaultConfig: {
+    timeout: 3000,
+  },
+  // 请求拦截
+  interceptorsRequest: (config: IConfig) => {
+    // 拦截处理请求配置
+    return config;
+  }
+  // 响应拦截
+  interceptorsResponse: (res: any, config: IConfig) => {
+    // 拦截处理响应数据
+    return res;
+  }
+});
+
+// 可省略基础路径，地址自动与 baseURL 拼接
+get('/test');
+```
+
+## 返回值
+
+请求结束后会返回 请求信息 和 请求结果 的合并对象，请求结果优先于请求信息
+
+```
+{
+  // 请求信息
+  time: {
+    start: '12:21:38', // 开始请求时间
+    end: '12:21:39', // 结束请求时间
+    total: '1秒', // 请求时长
+  },
+  error: 错误信息,
+  errorText: 错误文本解析,
+  // 请求结果
+  ...res,
+}
+```
+
+## 控制台打印日志
+
+```
+import FetchRequest, { log } from '@dyb881/fetch-request';
+
+const { get } = new FetchRequest({
+  // 请求拦截
+  interceptorsRequest: (config: IConfig) => {
+    // 打印日志
+    log.request(config);
+
+    // 拦截处理请求配置
+    return config;
+  }
+  // 响应拦截
+  interceptorsResponse: (res: any, config: IConfig) => {
+    res.ok = !res.errorText; // 请求结果状态 成功/失败
+
+    // 打印日志
+    log.response(res, config, res.ok);
+
+    // 拦截处理响应数据
+    return res;
+  }
+});
+```
+
+## 请求配置
+
+所有请求方法的参数都会合并到请求配置对象
+
+```
+interface IConfig {
+  mode?: 'same-origin' | 'no-cors' | 'cors' | 'navigate'; // 请求的模式
+  method?: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'; // 请求类型，部分后端只能识别大写
+  cache?: 'default' | 'no-store' | 'reload' | 'no-cache' | 'force-cache' | 'only-if-cached'; // 缓存模式
+  credentials?: 'omit' | 'same-origin' | 'include'; // 是否应该在来源请求中发送来自其他域的cookie
+  // 请求头
+  headers?: {
+    Accept?: string; // 期望得到数据格式
+    'Content-type'?: string; // 传递参数格式
+    [key: string]: any;
+  };
+  timeout?: number; // 请求超时
+  body?: any; // 请求主体
+  url?: string; // 请求地址
+  data?: any; // 请求元数据，转为主体前的数据
+  label?: string; // 请求标签，一般用于请求日志标记
+  [key: string]: any;
+}
+
+// 数据类型
+export const application = {
+  json: 'application/json', // json格式
+  form: 'application/x-www-form-urlencoded', // 表单对象格式
+};
+
+// 默认配置
+defaultConfig: IConfig = {
+  mode: 'cors',
+  method: 'GET',
+  cache: 'default',
+  credentials: 'omit',
+  headers: {
+    Accept: application.json,
+    'Content-type': application.json,
+  },
+  timeout: 3000,
+};
+
+```
+
+## 初始化配置
+
+new FetchRequest 时所需参数
+
+```
+interface IFetchRequestConfig {
+  host?: string; // API地址
+  apiPath?: string; // API目录
+  defaultConfig?: IConfig; // 默认配置
+  interceptorsRequest?: (config: IConfig) => IConfig; // 请求拦截，可以返回拦截处理的配置
+  interceptorsResponse?: (res: any, config: IConfig) => any; // 响应拦截，可以返回拦截处理的结果
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,272 @@
+import qs from 'qs';
+
+/**
+ * 默认请求配置
+ */
+interface IConfig {
+  mode?: 'same-origin' | 'no-cors' | 'cors' | 'navigate'; // 请求的模式
+  method?: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'; // 请求类型，部分后端只能识别大写
+  cache?: 'default' | 'no-store' | 'reload' | 'no-cache' | 'force-cache' | 'only-if-cached'; // 缓存模式
+  credentials?: 'omit' | 'same-origin' | 'include'; // 是否应该在来源请求中发送来自其他域的cookie
+  // 请求头
+  headers?: {
+    Accept?: string; // 期望得到数据格式
+    'Content-type'?: string; // 传递参数格式
+    [key: string]: any;
+  };
+  timeout?: number; // 请求超时
+  body?: any; // 请求主体
+  url?: string; // 请求地址
+  data?: any; // 请求元数据，转为主体前的数据
+  label?: string; // 请求标签，一般用于请求日志标记
+  [key: string]: any;
+}
+
+/**
+ * 初始化配置
+ */
+interface IFetchRequestConfig {
+  defaultConfig?: IConfig; // 默认配置
+  host?: string; // API地址
+  apiPath?: string; // API目录
+  interceptorsRequest?: (config: IConfig) => IConfig; // 请求拦截，可以返回拦截处理的配置
+  interceptorsResponse?: (res: any, config: IConfig) => any; // 响应拦截，可以返回拦截处理的结果
+}
+
+/**
+ * 数据类型
+ */
+export const application = {
+  json: 'application/json', // json格式
+  form: 'application/x-www-form-urlencoded', // 表单对象格式
+};
+
+/**
+ * 根据数据类型生成方法
+ */
+const applicationToBodyFun = {
+  [application.json]: JSON.stringify,
+  [application.form]: qs.stringify,
+};
+
+/**
+ * data 转为请求主体
+ */
+const toBody = (config: IConfig) => {
+  if (config.method === 'GET') {
+    const body = qs.stringify(config.data);
+    if (body) config.url += `?${body}`;
+  } else {
+    const contentType = (config.headers && config.headers['Content-type']) || application.json;
+    const body = applicationToBodyFun[contentType] && applicationToBodyFun[contentType](config.data);
+    if (body && ['{}', '[]'].indexOf(body) === -1) config.body = body;
+  }
+  return config;
+};
+
+/**
+ * 如果配置为文本类型，直接写入 label
+ */
+const labelToConfig = (config?: IConfig | string) => (typeof config === 'string' ? { label: config } : config);
+
+/**
+ * 对象数据写入表单对象
+ * 主要用于上传文件
+ */
+const getFormData = (data: object) => {
+  const body = new FormData();
+  const setData = (data: object, key = '') => {
+    Object.keys(data).forEach(i => {
+      const item = data[i];
+      if (item && typeof item === 'object' && !(item instanceof File)) {
+        setData(item, key ? `${key}[${i}]` : i);
+      } else {
+        body.append(key ? `${key}[${i}]` : i, item);
+      }
+    });
+  };
+  setData(data);
+  return body;
+};
+
+/**
+ * 统计时间
+ */
+const statisticalTime = () => {
+  const start = new Date();
+  return () => {
+    const end = new Date();
+    return {
+      start: start.toTimeString(),
+      end: end.toTimeString(),
+      total: `${(+end - +start) / 1000}秒`,
+    };
+  };
+};
+
+/**
+ * 错误解析字段
+ */
+const erroText = {
+  timeout: '网络连接超时',
+  'Network Error': '请求地址错误或跨域未允许',
+  'Failed to fetch': '请求地址错误或跨域未允许',
+};
+
+/**
+ * 异常分析 错误信息 => 错误解析文本
+ */
+const erroToText = (error: string): string => {
+  for (const reg in erroText) {
+    if (new RegExp(reg).test(error)) {
+      return erroText[reg];
+    }
+  }
+  return '其他错误';
+};
+
+/**
+ * 控制台打印颜色
+ */
+const consoleStyle = {
+  request: 'color: #0089E5;',
+  success: 'color: #2DB700;',
+  fail: 'color: #F41900;',
+};
+
+/**
+ * 打印日志
+ * 请求配置的 请求标签 label 代表打印日志的 文本内容
+ */
+export const log = {
+  request: (config: IConfig) => {
+    console.groupCollapsed(`%cRequest: ${config.label || config.url} ⇅`, consoleStyle.request);
+    console.log('请求类型：', config.method);
+    console.log('请求地址：', config.url);
+    console.log('请求数据：', config.data || '无');
+    console.log('请求配置：', config);
+    console.groupEnd();
+  },
+  response: (res: any, config: IConfig, success: boolean) => {
+    let title = `%cResponse: ${config.label || config.url} ${success ? '√' : '×'}`;
+    if (res.time && res.time.total) title += ` 用时：${res.time.total}`;
+    console.groupCollapsed(title, consoleStyle[success ? 'success' : 'fail']);
+    console.log('响应数据：', res);
+    if (!success) {
+      console.log('响应异常：', res.error || '无');
+      console.log('异常解析：', res.errorText);
+    }
+    console.groupEnd();
+  },
+};
+
+/**
+ * 请求模型
+ */
+export default class FetchRequest {
+  host = '';
+  apiPath = '';
+  defaultConfig: IConfig = {
+    mode: 'cors',
+    method: 'GET',
+    cache: 'default',
+    credentials: 'omit',
+    headers: {
+      Accept: application.json,
+      'Content-type': application.json,
+    },
+    timeout: 3000,
+  };
+  interceptorsRequest = (config: IConfig) => config;
+  interceptorsResponse = (res: any, _config: IConfig) => res;
+
+  constructor(config?: IFetchRequestConfig) {
+    if (config) {
+      const { defaultConfig, ...configs } = config;
+      Object.assign(this.defaultConfig, defaultConfig);
+      Object.assign(this, configs);
+    }
+  }
+
+  /**
+   * 基础路径
+   */
+  get baseURL() {
+    return this.host + this.apiPath;
+  }
+
+  /**
+   * 执行请求
+   */
+  request = (configs: IConfig) => {
+    let { url = '', ...config } = configs;
+
+    // 拼接地址
+    if (!/^http/.test(url)) url = this.baseURL + url;
+
+    // 转为主体，执行请求拦截
+    config = this.interceptorsRequest(
+      toBody({
+        url,
+        ...this.defaultConfig,
+        ...config,
+      })
+    );
+
+    // 开始统计时间
+    const st = statisticalTime();
+
+    // 发出请求
+    return Promise.race([
+      fetch(config.url, config), // 追加请求超时
+      new Promise((_, reject) => setTimeout(() => reject('request timeout'), config.timeout)),
+    ])
+      .then(response => {
+        if (!(response instanceof Response)) return response;
+        try {
+          return response.json(); // 默认 json 格式读取数据
+        } catch (e) {
+          return {
+            data: response.text(), // 失败则使用 text 格式读取
+          };
+        }
+      }) // 转化响应数据
+      .catch(error => ({
+        error,
+        errorText: erroToText(error),
+      }))
+      .then(res => this.interceptorsResponse({ time: st(), ...res }, config)); // 载入响应拦截
+  };
+
+  createRequest = (method: IConfig['method']) => (
+    url: string,
+    data?: object,
+    config?: IConfig | string,
+    coverConfig?: IConfig
+  ) =>
+    this.request({
+      method,
+      url,
+      data,
+      ...labelToConfig(config),
+      ...coverConfig,
+    });
+
+  get = this.createRequest('GET');
+  post = this.createRequest('POST');
+  put = this.createRequest('PUT');
+  patch = this.createRequest('PATCH');
+  del = this.createRequest('DELETE');
+  upload = (url: string, data: object, config?: IConfig | string, coverConfig?: IConfig) =>
+    this.request({
+      method: 'POST',
+      headers: {},
+      url,
+      data,
+      body: getFormData(data),
+      ...labelToConfig(config),
+      ...coverConfig,
+    });
+}
+
+export const { get, post, put, patch, del, upload } = new FetchRequest();
